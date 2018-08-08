232a8d0bbfb683c094d9784b74698f433de9403b
muqeet-khan <mohdabdulmuqeet@hotmail.com>
2018-08-07 19:27:35 -0500

    removed generic classes...

---
+++ src/BlazorComponents/ChartJS/ChartJSBarChart.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlazorComponents.ChartJS
+{
+    public class ChartJSBarChart
+    {
+        public string ChartType { get; set; } = "bar";
+        public ChartJsBarData Data { get; set; }
+        public ChartJsOptions Options { get; set; }
+        public string CanvasId { get; set; } = $"BlazorChartJS_{new Random().Next(0, 1000000).ToString()}";
+    }
+}
---
+++ src/BlazorComponents/ChartJS/ChartJSLineChart.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlazorComponents.ChartJS
+{
+    public class ChartJSLineChart
+    {
+        public string ChartType { get; set; } = "bar";
+        public ChartJsLineData Data { get; set; }
+        public ChartJsOptions Options { get; set; }
+        public string CanvasId { get; set; } = $"BlazorChartJS_{new Random().Next(0, 1000000).ToString()}";
+    }
+}
